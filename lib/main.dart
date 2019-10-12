356cce952839d58f9bd95990e7f0320ab3e1c8cd
Osvaldo <osva.00@gmail.com>
2019-10-11 19:11:59 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+main() => runApp(Controles());
+
+class Controles extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return Estado();
+  }
+
+}
+
+class Estado extends State {
+
+  double precio = 0.0;
+  double iva = 0.0;
+  double precioTotal = 0.0;
+  bool tienesMembresia = false;
+  bool palomitas = false;
+  bool cubetaPalomera = true;
+  String pelicula;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TextField'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.accessible_forward),
+                  labelText: 'Precio',
+                  hintText: 'Escribe el precio',
+                  helperText: 'Este precio no incluye el IVA',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                ),
+                onChanged: (cambio){
+                  // print(cambio);
+                  setState(() {
+                    precio = double.parse(cambio);
+                    iva = precio * 0.16;
+                    precioTotal = precio * 1.16;
+                  });
+                },
+              ),
+            ),
+            Text('Precio: $precio'),
+            Text('IVA: $iva'),
+            Text('Presio Total: $precioTotal'),
+            Checkbox(
+                value: tienesMembresia,
+                onChanged: (cambio) {
+                  setState(() {
+                    tienesMembresia = cambio;
+                  });
+                }
+            ),
+            Text('Tienes menbresia : $tienesMembresia'),
+            Switch(
+                value: palomitas,
+                onChanged: (cambio){
+                  setState(() {
+                    palomitas = cambio;
+                  });
+                }
+            ),
+            Text('¿Quieres palomitas? $palomitas'),
+            SwitchListTile(
+                value: cubetaPalomera,
+                title: Text('¿Quieres cubeta palomera?'),
+                subtitle: Text('Es del bromas, pelicula nueva de DC remasterizado'),
+                secondary: Icon(Icons.theaters),
+                isThreeLine: true,
+                onChanged: (cambio){
+                  setState(() {
+                    cubetaPalomera = cambio;
+                  });
+                }
+            ),
+            Text('¿Quiers cubeta? $cubetaPalomera'),
+            Row(
+              children: <Widget>[
+                Radio(
+                    value: 'ToyStory 4',
+                    groupValue: pelicula,
+                    onChanged: (cambio){
+                      setState(() {
+                        pelicula = cambio;
+                      });
+                    }
+                ),
+                Radio(
+                    value: 'El Bromas',
+                    groupValue: pelicula,
+                    onChanged: (cambio){
+                      setState(() {
+                        pelicula = cambio;
+                      });
+                    }
+                ),
+                Radio(
+                    value: 'Rey Leon',
+                    groupValue: pelicula,
+                    onChanged: (cambio){
+                      setState(() {
+                        pelicula = cambio;
+                      });
+                    }
+                )
+              ],
+            ),
+            Text('Tu pelicula es: $pelicula')
+          ],
+        ),
+      )
+    );
+  }
+}
